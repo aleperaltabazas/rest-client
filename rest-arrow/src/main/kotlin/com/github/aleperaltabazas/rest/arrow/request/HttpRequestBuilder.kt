8c2ce8166828bb1ad49579ccf4747d4360fff7f8
Alejandro <aleperaltabazas@hotmail.com>
2020-06-08 16:58:33 -0300

    fill request builder a bit

--- rest-arrow/src/main/kotlin/com/github/aleperaltabazas/rest/arrow/request/HttpRequestBuilder.kt
+++ rest-arrow/src/main/kotlin/com/github/aleperaltabazas/rest/arrow/request/HttpRequestBuilder.kt
@@ -1,10 +1,31 @@
 package com.github.aleperaltabazas.rest.arrow.request
 
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import arrow.fx.IO
 import com.github.aleperaltabazas.rest.core.request.BaseHttpRequestBuilder
 import com.github.aleperaltabazas.rest.core.request.BaseHttpRequestDecorator
 import com.github.aleperaltabazas.rest.core.response.HttpResponse
 
 class HttpRequestBuilder(
     request: BaseHttpRequestBuilder
-) : BaseHttpRequestDecorator(request)  {
+) : BaseHttpRequestDecorator(request) {
+    fun withBody(body: Any?): HttpRequestBuilder {
+        this.request.withBody(body)
+        return this
+    }
+
+    fun withHeader(header: String, value: String): HttpRequestBuilder {
+        this.request.withHeader(header, value)
+        return this
+    }
+
+    fun executeIO(): IO<HttpResponse> = IO { request.execute() }
+
+    fun executeEither(): Either<Throwable, HttpResponse> = try {
+        request.execute().right()
+    } catch (e: Throwable) {
+        e.left()
+    }
 }
